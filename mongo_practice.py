9f8f9a82b8551ea48a33be4c9180fce8846880ca
yuhaodai <93068257+B1uePumpkin@users.noreply.github.com>
2024-03-24 16:15:34 +0800

    Add ObjectId import and query data by ObjectId

--- mongo_practice.py
+++ mongo_practice.py
@@ -1,5 +1,6 @@
 # 載入pymongo套件
 import pymongo
+from bson.objectid import ObjectId
 # 連線到MongoDB
 
 client = pymongo.MongoClient("mongodb+srv://root:Password@mycluster.b0llbe5.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster")
@@ -13,8 +14,12 @@ collection = db.users # 選擇操作users集合
 # print("新增成功"+str(result.inserted_id))
 
 # 新增多筆資料
-result = collection.insert_many([{'name': 'David',
-                        'age': 30},
-                        {'name': 'Tom',
-                        'age': 25}])
-print("新增成功"+str(result.inserted_ids))
+# result = collection.insert_many([{'name': 'David',
+#                         'age': 30},
+#                         {'name': 'Tom',
+#                         'age': 25}])
+# print("新增成功"+str(result.inserted_ids))
+
+# 查詢資料
+result = collection.find_one(ObjectId("65ffbe51e10ff6aa7dcab083"))
+print(result)
